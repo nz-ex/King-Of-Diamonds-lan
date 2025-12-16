52a3071065efeb9c0be2f5bb1a678c324be1301f
nz-ex <frazzle21ondiscord@gmail.com>
2025-12-16 13:49:54 +0800

    Update start-lan.bat

--- start-lan.bat
+++ start-lan.bat
@@ -4,6 +4,7 @@ echo ========================================
 echo King of Diamonds LAN Multiplayer
 echo ========================================
 echo.
+npm install -D @vitejs/plugin-react
 
 REM Kill existing server
 echo [0/4] Clearing old server...
@@ -34,3 +35,4 @@ npm run dev -- --host 0.0.0.0
 
 echo [4/4] Ready!
 pause
+
